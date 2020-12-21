38cffb2259435771352b8be79420d7332a59631d
Jacob Drury <jacobdrury75@gmail.com>
2020-12-21 17:47:44 -0600

    push partials and intents

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { Client, Collection } = require('discord.js');
-const client = new Client();
+const client = new Client({
+    partials: ['MESSAGE', 'REACTION'],
+    intents: Intents.ALL,
+});
+
 ['commands', 'aliases'].forEach((x) => (client[x] = new Collection()));
 
 ['event', 'command'].forEach((handler) =>
