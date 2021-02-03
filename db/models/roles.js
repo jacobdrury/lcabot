9527b3d3fdfe3ef489349aa9680605bf24ab880f
Jacob Drury <jacobdrury75@gmail.com>
2021-02-03 13:18:54 -0600

    remove v1 code

--- db/models/roles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const rolesSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    Id: String,
-});
-
-module.exports = mongoose.model('Role', rolesSchema);
